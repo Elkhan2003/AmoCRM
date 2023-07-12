52bb6dd43921ae1dd80f6b1cda65d7dbe1455320
Elcho911 <boss.armsport@gmail.com>
2023-07-12 17:11:51 +0600

    Added the Vercel plugin

---
+++ api/serverless.ts
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Require the framework
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+	logger: false
+});
+
+// Register your application as a normal plugin.
+app.register(import("../src/index.ts"), {
+	prefix: "/"
+});
+
+export default async (req: FastifyRequest, res: FastifyReply) => {
+	await app.ready();
+	app.server.emit("request", req, res);
+};
