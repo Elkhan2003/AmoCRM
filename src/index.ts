6e0cf83e46c26626008b83ec6f163fda5818d4aa
Elcho911 <boss.armsport@gmail.com>
2023-07-16 18:13:43 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,8 @@ app.register(fastifyCors, {
 		"http://localhost:3000",
 		"http://127.0.0.1:3000",
 		"https://rest-api-amo-crm.vercel.app",
-		"https://rest-api-amocrm-production.up.railway.app"
+		"https://rest-api-amocrm-production.up.railway.app",
+		"https://server-production-374b.up.railway.app"
 	],
 	credentials: true
 });
@@ -22,7 +23,9 @@ app.register(routes, {
 	prefix: "/"
 });
 
-app.listen({ host: "localhost", port: 3000 }, (err, address) => {
+const PORT: any = process.env.PORT;
+
+app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
 		app.log.error(err);
 		process.exit(1);
