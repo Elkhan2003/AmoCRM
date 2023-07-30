1091bd55435be668d3b458a653e9983db516e4f1
Elcho911 <boss.armsport@gmail.com>
2023-07-31 03:37:02 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const start = async () => {
 			"http://localhost:3000",
 			"http://127.0.0.1:3000",
 			"https://amocrm-production.up.railway.app",
+			"https://wedevx911.vercel.app",
 		],
 		credentials: true,
 	});
