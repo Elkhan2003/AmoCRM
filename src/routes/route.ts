fec3d179088d0e2fc88deaaa1555bd2d91860bb1
Elcho911 <boss.armsport@gmail.com>
2023-08-01 13:19:22 +0600

    Update route.ts

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import controller from "../controllers/controller";
-// import otherControllers from "../controllers/other.controllers";
+import otherControllers from "../controllers/other.controllers";
 
 const routes = async (app: FastifyInstance) => {
 	app.get("/", controller.default);
@@ -8,5 +8,9 @@ const routes = async (app: FastifyInstance) => {
 	app.get("/get/:query", controller.getByQuery);
 	app.post("/post", controller.post);
 	app.patch("/patch", controller.patch);
+
+	// ! Test new functionality
+	app.post("/api/v1/send-sms", otherControllers.sendSmsCodeVerify);
+	app.get("/api/v1/support-connect", otherControllers.connectSupport);
 };
 export default routes;
