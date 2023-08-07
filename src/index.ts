c53b2bf6ac930f7ed2b29c96c2779e4b9c7dcc63
Elcho911 <boss.armsport@gmail.com>
2023-08-07 15:25:57 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const start = async () => {
 
 	server.register(fastifyCors, {
 		origin: [
+			"http://localhost:3000",
 			"http://localhost:5000",
 			"http://127.0.0.1:5000",
 			"https://amocrm-production.up.railway.app",
